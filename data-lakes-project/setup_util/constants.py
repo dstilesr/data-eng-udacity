4fc68e3e0e78d0ccfdf5eaa3ee071ac18502ab7d
dstilesr <d.stiles.r@hotmail.com>
2020-12-23 16:22:41 -0500

    Change config to use spot instances for workers

--- data-lakes-project/setup_util/constants.py
+++ data-lakes-project/setup_util/constants.py
@@ -2,12 +2,25 @@ DEFAULT_CONFIG: dict = {
     "Name": "MyCluster",
     "ReleaseLabel": "emr-6.0.0",
     "Instances": {
-        "InstanceCount": 2,
-        "MasterInstanceType": "m5.xlarge",
-        "SlaveInstanceType": "m5.xlarge",
         "AdditionalMasterSecurityGroups": [],
         "Ec2KeyName": "US-West-2-key",
-        "KeepJobFlowAliveWhenNoSteps": True
+        "KeepJobFlowAliveWhenNoSteps": True,
+        "InstanceGroups": [
+            {
+                "Name": "WorkersSpot",
+                "Market": "SPOT",
+                "InstanceType": "m5.xlarge",
+                "InstanceCount": 3,
+                "InstanceRole": "CORE"
+            },
+            {
+                "Name": "Master",
+                "Market": "ON_DEMAND",
+                "InstanceType": "m5.xlarge",
+                "InstanceCount": 1,
+                "InstanceRole": "MASTER"
+            }
+        ]
     },
     "Applications": [
         {"Name": "spark"},
