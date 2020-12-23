9188449030654ca70eae1ccc574f482c3986d03b
dstilesr <d.stiles.r@hotmail.com>
2020-12-23 15:56:38 -0500

    Code to process the logs dataset version 1

--- data-lakes-project/README.md
+++ data-lakes-project/README.md
@@ -16,9 +16,37 @@ processed data will be organized as a star schema to facilitate analytics.
 
 ## Repository Contents
 
+- The `etl.py` contains the code to perform the ETL process and generate the desired
+  output files. It can be run from the terminal or submitted to a Spark cluster.
+  
+- The `setup_util` package is a utility to quickly set up or take down an EMR cluster.
+  To use it, run
+  ```bash
+  python -m setup_util [create|delete]
+  ```
+  Running with `create` option will spin up the cluster, save some metadata in a
+  `cluster_meta.json` file and generate a `connection.bash` script which you can use
+  to ssh into the master node.
+
+- The `cfg-templates` directory contains templates for the `dl.cfg` file to use either locally
+  or on an AWS EMR cluster.
 
 ## Output Data Organization
 
+The output data is organized following a star schema. Since the main focus of the analytics
+group is songplays, the `songplays` table is the fact table. This fact table can be analyzed 
+along the following dimension tables:
+- `users`
+- `songs`
+- `artists`
+- `time`
+
+Each table is stored in a directory of the same name in the specified output location (which
+could be in the local filesystem, on S3, or even on HDFS) as a set of parquet files.
+
+Since the `songplays` and `time` tables are likely to be the largest and are likely to be joined 
+and filtered on often, they are partitioned by year and month for storage. Additionally, the
+`songs` table is partitioned by year and artist for storage.
 
 ## Running the Project
 
--- data-lakes-project/etl.py
+++ data-lakes-project/etl.py
@@ -3,8 +3,9 @@ import configparser
 from datetime import datetime
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import udf, col
+from pyspark.sql.types import TimestampType
 from pyspark.sql.functions import year, month
-from pyspark.sql.functions import dayofmonth, hour, weekofyear, date_format
+from pyspark.sql.functions import monotonically_increasing_id
 
 
 def create_spark_session():
@@ -46,12 +47,13 @@ def process_song_data(spark, input_data, output_data):
         "title",
         "case when year > 0 then year else null end as year",
         "duration"
-    ) # Year 0 is missing data!
+    )  # Year 0 is missing data!
 
     # write songs table to parquet files partitioned by year and artist
     songs_table.write \
+        .mode("overwrite") \
         .partitionBy("year", "artist_id") \
-        .parquet(output_data + "/songs")
+        .parquet(output_data + "songs")
 
     # extract columns to create artists table
     artists_table = df.selectExpr(
@@ -65,54 +67,118 @@ def process_song_data(spark, input_data, output_data):
         .dropDuplicates(subset=["artist_id"])  # One record per artist!
 
     # write artists table to parquet files
-    artists_table.write.parquet(output_data + "/artists")
+    artists_table.write \
+        .mode("overwrite") \
+        .parquet(output_data + "artists")
 
 
 def process_log_data(spark, input_data, output_data):
+    """
+    Processes the logs json files and stores the `time`, `users`, and `songplays`
+    tables as parquet files.
+    :param spark: Spark session.
+    :param input_data: Location of the input JSON dataset (S3 or local
+        filesystem address).
+    :param output_data: Location where output files will be stored (S3 or
+        local filesystem address).
+    """
     # get filepath to log data file
-    log_data =
+    log_data = input_data + "log_data/*.json"
 
     # read log data file
-    df =
-
-    # filter by actions for song plays
-    df =
+    df = spark.read\
+        .json(log_data)\
+        .where("page = 'NextSong'")\
+        .cache()
 
     # extract columns for users table
-    artists_table =
+    users_table = df \
+        .selectExpr(
+            "userId as user_id",
+            "firstName as first_name",
+            "lastName as last_name",
+            "gender",
+            "level"
+        ) \
+        .dropDuplicates(subset=["user_id"])  # Ensure one record per user
 
     # write users table to parquet files
-    artists_table
+    users_table.write\
+        .mode("overwrite")\
+        .parquet(output_data + "users")
 
     # create timestamp column from original timestamp column
-    get_timestamp = udf()
-    df =
-
-    # create datetime column from original timestamp column
-    get_datetime = udf()
-    df =
+    get_timestamp = udf(
+        lambda x: datetime.fromtimestamp(x // 1000),
+        TimestampType()
+    )
+    df = df.withColumn("start_time", get_timestamp(df.ts))
 
     # extract columns to create time table
-    time_table =
+    time_table = df.selectExpr(
+            "start_time",
+            "hour(start_time) as hour",
+            "dayofmonth(start_time) as day",
+            "weekofyear(start_time) as week",
+            "month(start_time) as month",
+            "year(start_time) as year",
+            "dayofweek(start_time) as weekday"
+        )\
+        .dropna(subset=["start_time"])\
+        .dropDuplicates(subset=["start_time"])
 
     # write time table to parquet files partitioned by year and month
-    time_table
+    time_table.write\
+        .mode("overwrite")\
+        .partitionBy("year", "month")\
+        .parquet(output_data + "time")
 
     # read in song data to use for songplays table
-    song_df =
+    song_df = spark.read.parquet(output_data + "songs") \
+        .select("title", "song_id", "artist_id") \
+        .join(
+            spark.read
+                .parquet(output_data + "artists")
+                .select("artist_id", "name"),
+            "artist_id",
+            "inner"
+        )\
+        .selectExpr("title as song", "name as artist", "song_id", "artist_id")
 
     # extract columns from joined song and log datasets to create songplays table
-    songplays_table =
+    songplays_table = df.join(song_df, ["song", "artist"], "left")\
+        .withColumn("songplay_id", monotonically_increasing_id())\
+        .selectExpr(
+            "songplay_id",
+            "start_time",
+            "userId as user_id",
+            "level",
+            "song_id",
+            "artist_id",
+            "sessionId as session_id",
+            "location",
+            "userAgent as user_agent"
+        )
 
     # write songplays table to parquet files partitioned by year and month
-    songplays_table
+    songplays_table\
+        .withColumn("year", year(songplays_table["start_time"]))\
+        .withColumn("month", month(songplays_table["start_time"]))\
+        .write\
+        .partitionBy("year", "month")\
+        .parquet(output_data + "songplays")
 
 
 def main():
+    """
+    Runs the ETL process.
+    :return:
+    """
     config = configparser.ConfigParser()
     config.read('dl.cfg')
 
     if "AWS" in config:
+        # Credentials are not necessary on a cluster (managed by IAM role)
         os.environ['AWS_ACCESS_KEY_ID'] = config["AWS"]['AWS_ACCESS_KEY_ID']
         os.environ['AWS_SECRET_ACCESS_KEY'] = config["AWS"]['AWS_SECRET_ACCESS_KEY']
 
